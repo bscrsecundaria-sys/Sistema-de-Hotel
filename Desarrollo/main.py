976ed127775db4283ddd8e96de053d00e0999e40
Brayan <bscrsecundaria@gmail.com>
2025-10-09 18:25:41 -0500

    Funcionamiento del menu iterativo

--- Desarrollo/main.py
+++ Desarrollo/main.py
@@ -1,15 +1,16 @@
+#Importacion de modulos
 from Modulos.clientes import Cliente
 from Modulos.habitaciones import Habitacion
 from Modulos.reservas import Reserva
 from Modulos.pagos import Pago
- 
-#Listas globales
+
+# Listas globales
 clientes = []
 habitaciones = []
 reservas = []
 pagos = []
 
-#Menú principal
+#Menu Iterativo
 while True:
     print("\nSISTEMA HOTELERO - MENÚ PRINCIPAL")
     print("1. Registrar cliente")
@@ -23,3 +24,47 @@ while True:
     print("9. Salir")
 
     opcion = input("\nSeleccione una opción: ")
+
+    if opcion == "1":
+        Cliente.registrar_cliente(clientes)
+
+    elif opcion == "2":
+        Habitacion.registrar_habitacion(habitaciones)
+
+    elif opcion == "3":
+        Habitacion.mostrar_disponibles(habitaciones)
+
+    elif opcion == "4":
+        Reserva.crear_reserva(clientes, habitaciones, reservas)
+
+    elif opcion == "5":
+        if not reservas:
+            print("No existen reservas registradas.")
+            continue
+        Reserva.consultar_reservas_cliente(reservas)
+
+    elif opcion == "6":
+        if not reservas:
+            print("No hay reservas para registrar pagos.")
+            continue
+        Pago.registrar_pago(pagos, reservas)
+
+    elif opcion == "7":
+        if not pagos:
+            print("No hay pagos registrados.")
+            continue
+        for p in pagos:
+            print(p)
+
+    elif opcion == "8":
+        if not reservas:
+            print("No hay reservas activas para finalizar.")
+            continue
+        Reserva.finalizar_reserva(reservas, habitaciones)
+
+    elif opcion == "9":
+        print("Saliendo del sistema...")
+        break
+
+    else:
+        print("Opción no válida. Intente de nuevo.")
