49b9c067b11a79b9b264217db2f188b3a5d787e4
Brayan <bscrsecundaria@gmail.com>
2025-10-10 15:36:02 -0500

    Verificación y funcionamiento: Habitaciones

--- Desarrollo/Modulos/habitaciones.py
+++ Desarrollo/Modulos/habitaciones.py
@@ -1,34 +1,47 @@
 class Habitacion:
-    #Dia 1:
-    def __init__(self, numero, tipo, tarifa, estado="disponible"):
+    # Día 1:
+    def _init_(self, numero, tipo, tarifa, estado="disponible"):
         self.numero = numero
         self.tipo = tipo
         self.tarifa = tarifa
         self.estado = estado
 
-    def __str__(self):
+    def _str_(self):
         return f"Habitación {self.numero} | Tipo: {self.tipo} | Tarifa: ${self.tarifa} | Estado: {self.estado}"
 
     def esta_disponible(self):
         return self.estado == "disponible"
 
-    #Dia 2:
+    # Día 2:
     def ocupar(self):
         self.estado = "ocupada"
 
     def liberar(self):
         self.estado = "disponible"
 
-    #Dia 3:
+    # Día 3:
     def registrar_habitacion(lista_habitaciones):
         print("\nRegistrar nueva habitación")
         numero = int(input("Número de habitación: "))
-        if any(h.numero == numero for h in lista_habitaciones):
-            print("Ya existe una habitación con ese número.")
-            return
+
+        for h in lista_habitaciones:
+            if h.numero == numero:
+                print("Ya existe una habitación con ese número.")
+                return
 
         tipo = input("Tipo de habitación (Sencilla/Doble/Suite): ")
         tarifa = float(input("Tarifa por noche: "))
+
         nueva = Habitacion(numero, tipo, tarifa)
         lista_habitaciones.append(nueva)
         print(f"\nHabitación {numero} registrada exitosamente.")
+
+    #Metodo para mostrar disponibles
+    def mostrar_disponibles(lista_habitaciones):
+        print("\nHabitaciones disponibles:")
+        disponibles = [h for h in lista_habitaciones if h.esta_disponible()]
+        if not disponibles:
+            print("No hay habitaciones disponibles.")
+            return
+        for h in disponibles:
+            print(h)
